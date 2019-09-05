e086f68f31502f844ee065bd20e264e8f48f65f8
Devlin D <webmasterdevlin>
2019-09-05 15:43:10 +0200

    change nav to gray

--- src/shared/HeaderNav.svelte
+++ src/shared/HeaderNav.svelte
@@ -27,7 +27,7 @@
   }
 </style>
 
-<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+<nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <Icon
       icon={faCube}
@@ -39,19 +39,7 @@
       Svelte Tour of Hereoes
     </span>
   </div>
-  <div class="block lg:hidden">
-    <button
-      class="flex items-center px-3 py-2 border rounded text-teal-200
-      border-teal-400 hover:text-white hover:border-white">
-      <svg
-        class="fill-current h-3 w-3"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg">
-        <title>Menu</title>
-        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-      </svg>
-    </button>
-  </div>
+
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
       <Link to="/">
